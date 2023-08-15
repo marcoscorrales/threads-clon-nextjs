e09a7222b4d917de03ed4da557418f6358fe3345
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-08-15 13:11:18 +0200

    back-end to update profile

---
+++ lib/mongoose.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // variable to check if mongoose is connected
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(!process.env.MONGODB_URL) return console.log('MONGO_URL not found');
+    if(isConnected) return console.log('Mongoose is already connected');
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        isConnected = true;
+        
+        console.log('Mongoose connected');
+    } catch (error) {
+        console.log(error);
+    }
+
+}
