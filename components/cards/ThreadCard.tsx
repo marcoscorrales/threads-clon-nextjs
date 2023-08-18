a03292ca7ad1dc55e860a3dae7986d96b12f4a39
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-08-18 19:03:24 +0200

    fetchPosts Homepage

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ThreadCard = ({
+  id,
+  currentUserId,
+  parentId,
+  content,
+  author,
+  community,
+  createdAt,
+  comments,
+}: Props) => {
+  return <div>ThreadCard</div>;
+};
+
+export default ThreadCard;
