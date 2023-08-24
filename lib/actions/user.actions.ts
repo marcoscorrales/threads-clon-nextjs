bdef05812b1e67068fd17446b5d1bada4ff82d26
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-08-24 20:25:16 +0200

    Activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { FilterQuery, SortOrder } from "mongoose";
+import { FilterQuery, SortOrder, model } from "mongoose";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
@@ -142,4 +142,32 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users: ${error.message}`)
   }
 
+}
+
+export async function getActivity( userId: string) {
+  try {
+    connectToDB();
+
+    // find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => { return acc.concat(userThread.children) 
+    }, [])  
+
+    const replies = await Thread.find({ _id: { $in: childThreadIds },
+    author: { $ne: userId }
+    }).populate({
+     path: 'author',
+     model: User,
+      select: 'name image _id'
+    })
+
+    return replies;
+    
+
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`)
+  }
 }
